29697367b0326d4b2273b4f6e78792a592839b80
Chris Nunes <chrisnunes57@gmail.com>
2020-09-14 00:05:09 -0500

    logging in works now

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 const cors = require("cors");
 const port = process.env.PORT || 5000
+const firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
+const config = require( "./firebase_config.js").config;
 
+firebase.initializeApp(config);
 app.use(cors());
 
 app.get('/', (req, res) => {
@@ -10,7 +15,33 @@ app.get('/', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  res.send({ message: "Hello, World!" });
+  let loginData = Buffer.from(
+    req.headers.authorization.split(" ")[1],
+    "base64"
+  ).toString();
+
+  loginData = loginData.split(":");
+
+  let email = loginData[0];
+  let password = loginData[1];
+
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    console.log(errorCode, errorMessage)
+    res.send({
+      "error": "Unable to sign in with these credentials, please try again."
+    });
+  }).then(data => {
+    if (data) {
+      console.log("successfully logged in!");
+      res.send({
+        "email": data.user.email,
+        "uid": data.user.uid
+      });
+    }
+  });
 })
 
 app.listen(port, () => {
