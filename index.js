d4fda4ac6e92c26a26eaabe39997493ee33bb795
Chris Nunes <chrisnunes57@gmail.com>
2020-09-13 19:49:22 -0500

    turned into basic express app

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const app = express()
+const port = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
