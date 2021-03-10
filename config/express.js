46d2bfd77341d59fe7d6af3a02d11c897e12bf73
Chris Nunes <chrisnunes57@gmail.com>
2021-03-10 10:55:43 -0600

    change /api endpoint to just /

--- config/express.js
+++ config/express.js
@@ -36,7 +36,7 @@ app.use(cors());
 // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // API router (routes beginning with "/api")
-app.use("/api", routes);
+app.use("/", routes);
 
 // Base backend route response
 app.get("/", (req, res) => {
